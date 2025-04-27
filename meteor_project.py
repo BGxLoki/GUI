6735dbda84f45eae94bb7dc835715777bb727522
BGxLoki <c3.wiley@gmail.com>
2025-04-27 14:47:21 -0400

    v2

--- meteor_project.py
+++ meteor_project.py
@@ -11,22 +11,46 @@ canvas = Canvas(window, width=map_width, height=map_height)
 canvas.create_image(0, 0, anchor=NW, image=bg)
 canvas.pack()
 
-longitude = 1
-latitude = 0
-
-f = open("meteor_strikes.csv")
+f = open("meteor_strikes.csv",encoding="utf-8")
 fileContents = f.read()
 lineList = fileContents.split("\n")
 
 def get_pixel_x(longitude):
     x_pixel = (longitude + 180) * (map_width / 360)
-    return x_pixel
+    return(x_pixel)
 
 def get_pixel_y(latitude):
     y_pixel = (85 - latitude) * (map_height / 170)
-    return y_pixel
+    return(y_pixel)
 
+def get_meteor_size(mass):
+    size = min(6, max(2, mass / 2000))
+    return(size)
 
+def get_period_color(year):
+    if (year < 500):
+        return("Orange")
+    elif (year >= 500 and year < 1500):
+        return("Red")
+    elif (year >= 1500 and year < 1900):
+        return("Green")
+    elif (year >= 1900):
+        return("Blue")
 
+def draw_meteor(latitude,longitude,mass,year,name):
+    x = get_pixel_x(longitude)
+    y = get_pixel_y(latitude)
+    size = get_meteor_size(mass)
+    color = get_period_color(year)
+    radius = size / 2
+    canvas.create_oval(x-radius,y-radius,x+radius,y+radius, outline = color, width = 1)
+    if (mass > 10000000):
+        canvas.create_text(x,y,text = name,font = ("Arial", 10),fill = 'gold')
 
+def draw_all_meteors():
+    for line in range(0,len(lineList)-1):
+        lineSplit = lineList.split(",")
+        draw_meteor(float(lineSplit[4]),float(lineSplit[3]),float(lineSplit[1]),int(lineSplit[2]),str(lineSplit[0]))
+print(lineList)
+draw_all_meteors()
 window.mainloop()
